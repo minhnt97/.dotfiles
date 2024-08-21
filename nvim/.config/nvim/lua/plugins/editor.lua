f053f5f3fafd4926f3ce110a6ac513d8c6a3b29d
Minh Nguyen <minh.nguyenthanh97@gmail.com>
2024-08-21 23:17:47 +0900

    add background image & remove specific settings for monitors

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -1,24 +1,25 @@
 return {
-	{
-		"kylechui/nvim-surround",
-		version = "*",
-		event = "VeryLazy",
-		config = function()
-			require("nvim-surround").setup({})
-		end,
-	},
-	{
-		"alexghergh/nvim-tmux-navigation",
-		config = function()
-			require("nvim-tmux-navigation").setup({
-				disable_when_zoomed = true,
-			})
-		end,
-	},
-	{
-		"mbbill/undotree",
-		config = function()
-			vim.keymap.set("n", "<leader>u", "<cmd>UndotreeToggle<cr>")
-		end,
-	},
+    {
+        "kylechui/nvim-surround",
+        version = "*",
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({})
+        end,
+    },
+    {
+        "alexghergh/nvim-tmux-navigation",
+        config = function()
+            require("nvim-tmux-navigation").setup({
+                disable_when_zoomed = true,
+            })
+        end,
+    },
+    {
+        "mbbill/undotree",
+        config = function()
+            vim.g.undotree_WindowLayout = 2
+            vim.keymap.set("n", "<leader>u", "<cmd>UndotreeToggle<cr>")
+        end,
+    },
 }
