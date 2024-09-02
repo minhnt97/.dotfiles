2a29acff39f49d25354a65319a1760ede0dff016
Minh Nguyen <minh.nguyenthanh97@gmail.com>
2024-09-02 15:21:43 +0900

    use full path for file name in nvim line

--- nvim/.config/nvim/lua/plugins/appearance.lua
+++ nvim/.config/nvim/lua/plugins/appearance.lua
@@ -1,20 +1,28 @@
 return {
-	{
-		"rebelot/kanagawa.nvim",
-		config = function()
-			require("kanagawa").setup({})
-			vim.cmd("colorscheme kanagawa-wave")
-		end,
-	},
-	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			require("lualine").setup({
-				options = {
-					theme = "auto",
-				},
-			})
-		end,
-	},
+    {
+        "rebelot/kanagawa.nvim",
+        config = function()
+            require("kanagawa").setup({})
+            vim.cmd("colorscheme kanagawa-wave")
+        end,
+    },
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require("lualine").setup({
+                options = {
+                    theme = "auto",
+                },
+                sections = {
+                    lualine_c = {
+                        {
+                            "filename",
+                            path = 1, -- 1: Relative path
+                        },
+                    },
+                },
+            })
+        end,
+    },
 }
