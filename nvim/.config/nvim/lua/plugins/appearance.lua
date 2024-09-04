e55994b14f8e40148c33f8ea9f70bead2e9db5d9
Minh Nguyen <minh.nguyenthanh97@gmail.com>
2024-09-04 20:53:52 +0900

    use gruvbox theme

--- nvim/.config/nvim/lua/plugins/appearance.lua
+++ nvim/.config/nvim/lua/plugins/appearance.lua
@@ -1,28 +1,35 @@
 return {
-    {
-        "rebelot/kanagawa.nvim",
-        config = function()
-            require("kanagawa").setup({})
-            vim.cmd("colorscheme kanagawa-dragon")
-        end,
-    },
-    {
-        "nvim-lualine/lualine.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = function()
-            require("lualine").setup({
-                options = {
-                    theme = "auto",
-                },
-                sections = {
-                    lualine_c = {
-                        {
-                            "filename",
-                            path = 1, -- 1: Relative path
-                        },
-                    },
-                },
-            })
-        end,
-    },
+	{
+		"sainnhe/gruvbox-material",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			-- Optionally configure and load the colorscheme
+			-- directly inside the plugin declaration.
+			vim.g.background = "dark"
+			vim.g.gruvbox_material_background = "hard"
+			vim.g.gruvbox_material_enable_italic = true
+			vim.g.gruvbox_material_better_performance = true
+			vim.cmd.colorscheme("gruvbox-material")
+		end,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("lualine").setup({
+				options = {
+					theme = "auto",
+				},
+				sections = {
+					lualine_c = {
+						{
+							"filename",
+							path = 1, -- 1: Relative path
+						},
+					},
+				},
+			})
+		end,
+	},
 }
