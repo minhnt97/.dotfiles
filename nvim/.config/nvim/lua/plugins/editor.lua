12c231c0d83037b6faef97c1b791c622969cd285
Minh Nguyen <minh.nguyenthanh97@gmail.com>
2024-10-08 12:35:16 +0900

    use quickfix preview

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -1,46 +1,46 @@
 return {
-	{
-		"kylechui/nvim-surround",
-		version = "*",
-		event = "VeryLazy",
-		config = function()
-			require("nvim-surround").setup({})
-		end,
-	},
-	{
-		"windwp/nvim-autopairs",
-		event = "InsertEnter",
-		config = true,
-		-- use opts = {} for passing setup options
-		-- this is equivalent to setup({}) function
-	},
-	{
-		"alexghergh/nvim-tmux-navigation",
-		config = function()
-			require("nvim-tmux-navigation").setup({
-				disable_when_zoomed = true,
-			})
-		end,
-	},
-	{
-		"mbbill/undotree",
-		config = function()
-			vim.g.undotree_WindowLayout = 2
-			vim.keymap.set("n", "<leader>u", "<cmd>UndotreeToggle<cr>")
-		end,
-	},
-	{
-		"sindrets/diffview.nvim",
-		config = function()
-			vim.keymap.set("n", "<leader>gs", "<cmd>DiffviewOpen<cr>")
-			vim.keymap.set("n", "<leader>gc", "<cmd>DiffviewClose<cr>")
-		end,
-	},
-	{
-		"nvimdev/hlsearch.nvim",
-		event = "BufRead",
-		config = function()
-			require("hlsearch").setup()
-		end,
-	},
+        {
+                "kylechui/nvim-surround",
+                version = "*",
+                event = "VeryLazy",
+                config = function()
+                        require("nvim-surround").setup({})
+                end,
+        },
+        {
+                "windwp/nvim-autopairs",
+                event = "InsertEnter",
+                config = true,
+                -- use opts = {} for passing setup options
+                -- this is equivalent to setup({}) function
+        },
+        {
+                "alexghergh/nvim-tmux-navigation",
+                config = function()
+                        require("nvim-tmux-navigation").setup({
+                                disable_when_zoomed = true,
+                        })
+                end,
+        },
+        {
+                "mbbill/undotree",
+                config = function()
+                        vim.g.undotree_WindowLayout = 2
+                        vim.keymap.set("n", "<leader>u", "<cmd>UndotreeToggle<cr>")
+                end,
+        },
+        {
+                "sindrets/diffview.nvim",
+                config = function()
+                        vim.keymap.set("n", "<leader>gs", "<cmd>DiffviewOpen<cr>")
+                        vim.keymap.set("n", "<leader>gc", "<cmd>DiffviewClose<cr>")
+                end,
+        },
+        {
+                "nvimdev/hlsearch.nvim",
+                event = "BufRead",
+                config = function()
+                        require("hlsearch").setup()
+                end,
+        },
 }
