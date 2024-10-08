d5acd3862e919611e02c350bfaca9e3c44da1d03
Minh Nguyen <minh.nguyenthanh97@gmail.com>
2024-10-08 11:18:21 +0900

    use dashboard

--- nvim/.config/nvim/lua/plugins/appearance.lua
+++ nvim/.config/nvim/lua/plugins/appearance.lua
@@ -1,35 +1,45 @@
 return {
-    {
-        "sainnhe/gruvbox-material",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            -- Optionally configure and load the colorscheme
-            -- directly inside the plugin declaration.
-            vim.o.background = "dark"
-            vim.g.gruvbox_material_background = "light"
-            vim.g.gruvbox_material_enable_italic = true
-            vim.g.gruvbox_material_better_performance = true
-            vim.cmd.colorscheme("gruvbox-material")
-        end,
-    },
-    {
-        "nvim-lualine/lualine.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = function()
-            require("lualine").setup({
-                options = {
-                    theme = "auto",
-                },
-                sections = {
-                    lualine_c = {
-                        {
-                            "filename",
-                            path = 1, -- 1: Relative path
-                        },
-                    },
-                },
-            })
-        end,
-    },
+	{
+		"sainnhe/gruvbox-material",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			-- Optionally configure and load the colorscheme
+			-- directly inside the plugin declaration.
+			vim.o.background = "dark"
+			vim.g.gruvbox_material_background = "light"
+			vim.g.gruvbox_material_enable_italic = true
+			vim.g.gruvbox_material_better_performance = true
+			vim.cmd.colorscheme("gruvbox-material")
+		end,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("lualine").setup({
+				options = {
+					theme = "auto",
+				},
+				sections = {
+					lualine_c = {
+						{
+							"filename",
+							path = 1, -- 1: Relative path
+						},
+					},
+				},
+			})
+		end,
+	},
+	{
+		"nvimdev/dashboard-nvim",
+		event = "VimEnter",
+		config = function()
+			require("dashboard").setup({
+				-- config
+			})
+		end,
+		dependencies = { { "nvim-tree/nvim-web-devicons" } },
+	},
 }
