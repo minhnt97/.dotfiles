9636165184252aa8c5431832896afd663fe4b88a
Minh Nguyen <minh.nguyenthanh97@gmail.com>
2024-10-24 17:54:36 +0900

    correct keymaps & telescope extension

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -39,55 +39,18 @@ return {
 			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
 			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
 			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "]d", vim.diagnostic.goto_next, {})
+			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, {})
+			vim.keymap.set("n", "<leader>ge", vim.diagnostic.setqflist, {})
 		end,
-	},
-	{
-		-- diagnostics navigate
-		"folke/trouble.nvim",
-		opts = {
-			modes = {
-				lsp_base = {
-					params = {
-						include_current = true,
-					},
-				},
-			},
-		}, -- for default options, refer to the configuration section for custom setup.
-		cmd = "Trouble",
-		keys = {
-			{
-				"<leader>ge",
-				"<cmd>Trouble diagnostics toggle focus=true<cr>",
-				desc = "Diagnostics (Trouble)",
-			},
-			{
-				"<leader>gE",
-				"<cmd>Trouble diagnostics toggle focus=true filter.buf=0<cr>",
-				desc = "Buffer Diagnostics (Trouble)",
-			},
-			{
-				"<leader>gr",
-				"<cmd>Trouble lsp_references toggle focus=true win.position=bottom<cr>",
-				desc = "LSP references (Trouble)",
-			},
-			{
-				"<leader>xl",
-				"<cmd>Trouble loclist toggle focus=true<cr>",
-				desc = "Location List (Trouble)",
-			},
-			{
-				"<leader>xq",
-				"<cmd>Trouble qflist toggle focus=true<cr>",
-				desc = "Quickfix List (Trouble)",
-			},
-		},
 	},
 	{
 		-- prettier diagnostics display
 		"rachartier/tiny-inline-diagnostic.nvim",
-		event = "VeryLazy", -- Or `LspAttach`
+		event = "LspAttach", -- Or `LspAttach`
 		config = function()
 			vim.diagnostic.config({ virtual_text = false })
 			require("tiny-inline-diagnostic").setup()
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,158 +1,130 @@
 return {
-        {
-                "nvim-telescope/telescope.nvim",
-                tag = "0.1.8",
-                dependencies = { "nvim-lua/plenary.nvim" },
-                config = function()
-                        local telescope = require("telescope")
-                        local telescope_builtin = require("telescope.builtin")
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{
+				"nvim-telescope/telescope-fzf-native.nvim",
+				build = "make",
+			},
+			{
+				"nvim-telescope/telescope-live-grep-args.nvim",
+				-- This will not install any breaking changes.
+				-- For major updates, this must be adjusted manually.
+				version = "^1.0.0",
+			},
+			"nvim-telescope/telescope-ui-select.nvim",
+			"nvim-telescope/telescope-file-browser.nvim",
+		},
+		config = function()
+			local telescope = require("telescope")
+			local telescope_builtin = require("telescope.builtin")
+			local lga_actions = require("telescope-live-grep-args.actions")
+			local live_grep_args_shortcuts = require("telescope-live-grep-args.shortcuts")
 
-                        -- mappings for file & text search
-                        vim.keymap.set("n", "<leader>ff", function()
-                                telescope_builtin.find_files({
-                                        hidden = true,
-                                        no_ignore = true,
-                                })
-                        end, {})
-                        vim.keymap.set("n", "<leader>fb", telescope_builtin.buffers, {})
-                        vim.keymap.set("n", "<leader>fw", telescope_builtin.grep_string, {})
-                        vim.keymap.set("n", "<leader>fo", telescope_builtin.oldfiles, {})
-                        vim.keymap.set("n", "<leader>fr", telescope_builtin.registers, {})
+			-- mappings for file search
+			vim.keymap.set("n", "<leader>ff", function()
+				telescope_builtin.find_files({
+					hidden = true,
+					no_ignore = true,
+				})
+			end, {})
+			vim.keymap.set("n", "<leader>fe", function()
+				require("telescope").extensions.file_browser.file_browser()
+			end, {})
+			vim.keymap.set("n", "<leader>fb", telescope_builtin.buffers, {})
+			vim.keymap.set("n", "<leader>fo", telescope_builtin.oldfiles, {})
 
-                        -- mappings for symbols search using LSP
-                        vim.keymap.set("n", "<leader>fs", telescope_builtin.lsp_document_symbols, {})
-                        vim.keymap.set("n", "<leader>fa", telescope_builtin.lsp_dynamic_workspace_symbols, {})
+			-- mappings for text search
+			vim.keymap.set("n", "<leader>fg", function()
+				telescope.extensions.live_grep_args.live_grep_args({})
+			end, {})
+			vim.keymap.set("n", "<leader>fw", function()
+				live_grep_args_shortcuts.grep_word_under_cursor({
+					postfix = "",
+					qoute = false,
+				})
+			end, {})
 
-                        -- connect with trouble
-                        local actions = require("telescope.actions")
-                        local open_with_trouble = require("trouble.sources.telescope").open
-                        -- Use this to add more results without clearing the trouble list
-                        local add_to_trouble = require("trouble.sources.telescope").add
+			-- mappings for vim
+			vim.keymap.set("n", "<leader>fr", telescope_builtin.registers, {})
+			vim.keymap.set("n", "<leader>fm", telescope_builtin.keymaps, {})
 
-                        telescope.setup({
-                                defaults = {
-                                        sorting_strategy = "ascending",
-                                        layout_strategy = "vertical",
-                                        layout_config = {
-                                                vertical = {
-                                                        prompt_position = "top",
-                                                },
-                                        },
-                                        mappings = {
-                                                i = { ["<c-t>"] = open_with_trouble },
-                                                n = { ["<c-t>"] = open_with_trouble },
-                                        },
-                                },
-                                pickers = {
-                                        find_files = {
-                                                mappings = {
-                                                        n = {
-                                                                ["cd"] = function(prompt_bufnr)
-                                                                        local selection = require(
-                                                                        "telescope.actions.state").get_selected_entry()
-                                                                        local dir = vim.fn.fnamemodify(selection.path,
-                                                                                ":p:h")
-                                                                        require("telescope.actions").close(prompt_bufnr)
-                                                                        -- Depending on what you want put `cd`, `lcd`, `tcd`
-                                                                        vim.cmd(string.format("silent lcd %s", dir))
-                                                                end,
-                                                        },
-                                                },
-                                        },
-                                        lsp_document_symbols = {
-                                                symbol_width = 50,
-                                        },
-                                        lsp_dynamic_workspace_symbols = {
-                                                fname_width = 50,
-                                        },
-                                        lsp_references = {
-                                                fname_width = 50,
-                                                include_current_line = true,
-                                                trim_text = true,
-                                        },
-                                },
-                        })
-                end,
-        },
-        {
-                "nvim-telescope/telescope-fzf-native.nvim",
-                build = "make",
-                config = function()
-                        require("telescope").load_extension("fzf")
-                end,
-        },
-        {
-                "nvim-telescope/telescope-ui-select.nvim",
-                config = function()
-                        require("telescope").setup({
-                                extensions = {
-                                        ["ui-select"] = {
-                                                require("telescope.themes").get_dropdown({}),
-                                        },
-                                },
-                        })
-                        require("telescope").load_extension("ui-select")
-                end,
-        },
-        {
-                "nvim-telescope/telescope-file-browser.nvim",
-                dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-                config = function()
-                        local telescope = require("telescope")
+			-- mappings for symbols search using LSP
+			vim.keymap.set("n", "<leader>fs", telescope_builtin.lsp_document_symbols, {})
+			vim.keymap.set("n", "<leader>fa", telescope_builtin.lsp_dynamic_workspace_symbols, {})
 
-                        telescope.setup({
-                                extensions = {
-                                        file_browser = {
-                                                theme = "ivy",
-                                        },
-                                },
-                        })
+			-- setup everything
+			telescope.setup({
+				defaults = {
+					sorting_strategy = "ascending",
+					layout_strategy = "vertical",
+					layout_config = {
+						vertical = {
+							prompt_position = "top",
+						},
+					},
+				},
+				pickers = {
+					find_files = {
+						mappings = {
+							n = {
+								["cd"] = function(prompt_bufnr)
+									local selection = require("telescope.actions.state").get_selected_entry()
+									local dir = vim.fn.fnamemodify(selection.path, ":p:h")
+									require("telescope.actions").close(prompt_bufnr)
+									-- Depending on what you want put `cd`, `lcd`, `tcd`
+									vim.cmd(string.format("silent lcd %s", dir))
+								end,
+							},
+						},
+					},
+					lsp_document_symbols = {
+						symbol_width = 50,
+					},
+					lsp_dynamic_workspace_symbols = {
+						fname_width = 50,
+					},
+					lsp_references = {
+						fname_width = 50,
+						include_current_line = true,
+						trim_text = true,
+					},
+				},
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+					file_browser = {
+						theme = "ivy",
+					},
+					live_grep_args = {
+						auto_quoting = true, -- enable/disable auto-quoting
+						mappings = { -- extend mappings
+							i = {
+								["<C-k>"] = lga_actions.quote_prompt(),
+								["<C-h>"] = lga_actions.quote_prompt({
+									postfix = " --hidden --no-ignore ",
+								}),
+								["<C-g>"] = lga_actions.quote_prompt({
+									postfix = " --iglob ",
+								}),
+								["<C-t>"] = lga_actions.quote_prompt({
+									postfix = " -t ",
+								}),
+								-- freeze the current list and start a fuzzy search in the frozen list
+								["<C-space>"] = lga_actions.to_fuzzy_refine,
+							},
+						},
+					},
+				},
+			})
 
-                        vim.keymap.set("n", "<leader>fe", function()
-                                require("telescope").extensions.file_browser.file_browser()
-                        end, {})
-
-                        telescope.load_extension("file_browser")
-                end,
-        },
-        {
-                "nvim-telescope/telescope-live-grep-args.nvim",
-                -- This will not install any breaking changes.
-                -- For major updates, this must be adjusted manually.
-                version = "^1.0.0",
-                config = function()
-                        local telescope = require("telescope")
-                        local lga_actions = require("telescope-live-grep-args.actions")
-
-                        telescope.setup({
-                                extensions = {
-                                        live_grep_args = {
-                                                auto_quoting = true, -- enable/disable auto-quoting
-                                                -- define mappings, e.g.
-                                                mappings = { -- extend mappings
-                                                        i = {
-                                                                ["<C-k>"] = lga_actions.quote_prompt(),
-                                                                ["<C-i>"] = lga_actions.quote_prompt({
-                                                                        postfix = " --iglob ",
-                                                                }),
-                                                                -- freeze the current list and start a fuzzy search in the frozen list
-                                                                -- ["<C-space>"] = actions.to_fuzzy_refine,
-                                                        },
-                                                },
-                                                -- ... also accepts theme settings, for example:
-                                                -- theme = "dropdown", -- use dropdown theme
-                                                -- theme = { }, -- use own theme spec
-                                                -- layout_config = { mirror=true }, -- mirror preview pane
-                                        },
-                                },
-                        })
-
-                        vim.keymap.set("n", "<leader>fg", function()
-                                telescope.extensions.live_grep_args.live_grep_args()
-                        end, {})
-
-                        -- then load the extension
-                        telescope.load_extension("live_grep_args")
-                end,
-        },
+			telescope.load_extension("fzf")
+			telescope.load_extension("live_grep_args")
+			telescope.load_extension("ui-select")
+			telescope.load_extension("file_browser")
+		end,
+	},
 }
