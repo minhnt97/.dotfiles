bf67e32f37556ff623d73b0eac428e16b22aa721
Minh Nguyen <minh.nguyenthanh97@gmail.com>
2024-11-21 00:00:57 +0900

    use Source Code Pro Medium & use auto resize windows nvim

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -18,6 +18,7 @@ return {
 		end,
 	},
 	{
+		-- auto resize windows with animation
 		"anuvyklack/windows.nvim",
 		dependencies = {
 			"anuvyklack/middleclass",
@@ -27,6 +28,15 @@ return {
 			vim.o.winwidth = 10
 			vim.o.winminwidth = 10
 			vim.o.equalalways = false
+
+			local function cmd(command)
+				return table.concat({ "<Cmd>", command, "<CR>" })
+			end
+
+			vim.keymap.set("n", "<C-w>z", cmd("WindowsMaximize"))
+			vim.keymap.set("n", "<C-w>_", cmd("WindowsMaximizeVertically"))
+			vim.keymap.set("n", "<C-w>|", cmd("WindowsMaximizeHorizontally"))
+			vim.keymap.set("n", "<C-w>=", cmd("WindowsEqualize"))
 			require("windows").setup()
 		end,
 	},
