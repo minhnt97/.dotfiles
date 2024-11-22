712b432f4781cd857bca0f856aa57683f223d804
Minh Nguyen <minh.nguyenthanh97@gmail.com>
2024-11-23 00:28:32 +0900

    auto focus on undotree

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -53,6 +53,7 @@ return {
 		"mbbill/undotree",
 		config = function()
 			vim.g.undotree_WindowLayout = 2
+			vim.g.undotree_SetFocusWhenToggle = 1
 			vim.keymap.set("n", "<leader>u", "<cmd>UndotreeToggle<cr>")
 		end,
 	},
