5f0e40cbe166e3a1cda6c59985567099464de6fe
Minh Nguyen <minh.nguyenthanh97@gmail.com>
2024-11-25 15:55:49 +0900

    correct debug continue

--- nvim/.config/nvim/lua/plugins/debug.lua
+++ nvim/.config/nvim/lua/plugins/debug.lua
@@ -13,7 +13,10 @@ return {
 
 			-- mappings for debug session control
 			vim.keymap.set("n", "<Leader>dc", function()
-				dap.continue()
+				if vim.fn.filereadable(".vscode/launch.json") then
+					require("dap.ext.vscode").load_launchjs()
+				end
+				require("dap").continue()
 			end)
 			vim.keymap.set("n", "<Leader>dr", function()
 				dap.restart()
@@ -48,9 +51,34 @@ return {
 			end)
 
 			-- mappings for watch related actions
-			vim.keymap.set("n", "<Leader>dk", function()
-				dapui.eval()
-			end)
+			local api = vim.api
+			local keymap_restore = {}
+			dap.listeners.after["event_initialized"]["me"] = function()
+				for _, buf in pairs(api.nvim_list_bufs()) do
+					local keymaps = api.nvim_buf_get_keymap(buf, "n")
+					for _, keymap in pairs(keymaps) do
+						if keymap.lhs == "K" then
+							table.insert(keymap_restore, keymap)
+							api.nvim_buf_del_keymap(buf, "n", "K")
+						end
+					end
+				end
+				api.nvim_set_keymap("n", "K", '<Cmd>lua require("dap.ui.widgets").hover()<CR>', { silent = true })
+			end
+
+			dap.listeners.after["event_terminated"]["me"] = function()
+				for _, keymap in pairs(keymap_restore) do
+					api.nvim_buf_set_keymap(
+						keymap.buffer,
+						keymap.mode,
+						keymap.lhs,
+						keymap.rhs,
+						{ silent = keymap.silent == 1 }
+					)
+				end
+				keymap_restore = {}
+			end
+
 			vim.keymap.set("n", "<Leader>ds", function()
 				dapui.float_element("scopes", {
 					enter = true,
