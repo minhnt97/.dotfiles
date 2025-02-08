f35051585fd5431597ded2ed804c075bd422f6da
Minh Nguyen <minh.nguyenthanh97@gmail.com>
2025-02-08 17:36:15 +0700

    use diff command instead

--- nvim/.config/nvim/lua/config/after_init.lua
+++ nvim/.config/nvim/lua/config/after_init.lua
@@ -3,7 +3,3 @@ vim.o.termguicolors = true
 
 -- Set colorscheme
 vim.cmd.colorscheme("nordic")
-
-if vim.fn.has("win32") == 1 then -- only windows
-        vim.g.undotree_DiffCommand = "~/.dotfiles/nvim/diff.exe"
-end
