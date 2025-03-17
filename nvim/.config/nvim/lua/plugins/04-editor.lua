3ff59207fe42ff1b58355ce2198b65a173db3fa8
Minh Nguyen <minh.nguyenthanh97@gmail.com>
2025-03-17 21:49:21 +0700

    correct settings

--- nvim/.config/nvim/lua/plugins/04-editor.lua
+++ nvim/.config/nvim/lua/plugins/04-editor.lua
@@ -70,6 +70,7 @@ return {
                 config = function()
                         require("peek").setup({
                                 app = 'browser',
+                                theme = 'light',
                         })
                         vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
                         vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
