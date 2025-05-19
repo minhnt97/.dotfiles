bd5dbe61b132086928b0117d44bddd1fa47b4eb3
Minh Nguyen <minh.nguyenthanh97@gmail.com>
2025-05-19 21:26:14 +0700

    add pwsh

---
+++ powershell/custom.ps1
@@ -0,0 +1,12 @@
+# Shows navigable menu of all options when hitting Tab
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+# Autocompletion for arrow keys
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+# Install posh-git for git autocompletion
+Import-Module posh-git
+
+# Load prompt theme
+oh-my-posh init pwsh --config ~/.dotfiles/oh-my-posh/custom.json | Invoke-Expression
