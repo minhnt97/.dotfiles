fa351d1653e7664311fc680052d03b4e627ccfab
Minh Nguyen <minh.nguyenthanh97@gmail.com>
2025-07-24 23:49:43 +0700

    try blink.cmp

--- nvim/.config/nvim/lua/plugins/02-autocompletion.lua
+++ nvim/.config/nvim/lua/plugins/02-autocompletion.lua
@@ -1,157 +1,126 @@
 return {
-        {
-                "hrsh7th/nvim-cmp",
-                dependencies = {
-                        "hrsh7th/cmp-buffer",                   -- buffer source for nvim-cmp
-                        "hrsh7th/cmp-path",                     -- path source for nvim-cmp
-                        "hrsh7th/cmp-cmdline",                  -- path source for nvim-cmp
-                        "neovim/nvim-lspconfig",
-                        "hrsh7th/cmp-nvim-lsp",                 -- LSP source for nvim-cmp
-                        "hrsh7th/cmp-nvim-lsp-document-symbol", -- display signature help for nvim-cmp
-                        "hrsh7th/cmp-nvim-lsp-signature-help",  -- display signature help for nvim-cmp
-                        "rafamadriz/friendly-snippets",         -- vs-code like snippets
-                        "saadparwaiz1/cmp_luasnip",             -- LuaSnip source for nvim-cmp
-                        "L3MON4D3/LuaSnip",                     -- snippet engine
-                        "onsails/lspkind.nvim",                 -- add vscode-like icon to completion menu
-                        "rcarriga/cmp-dap",                     -- for dap autocompletion
-                },
-                config = function()
-                        -- luasnip setup
-                        local luasnip = require("luasnip")
+	{
+		"saghen/blink.cmp",
 
-                        -- lspkind setup
-                        local lspkind = require("lspkind")
+		dependencies = {
+			"rafamadriz/friendly-snippets", --snippets
+			"xzbdmw/colorful-menu.nvim", --extending label
+			"nvim-tree/nvim-web-devicons", --icons
+			"onsails/lspkind.nvim", --icons
+		},
 
-                        -- nvim-cmp setup
-                        local cmp = require("cmp")
+		-- use a release tag to download pre-built binaries
+		version = "1.*",
+		-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+		-- build = 'cargo build --release',
+		-- If you use nix, you can build from source using latest nightly rust with:
+		-- build = 'nix run .#build-plugin',
 
-                        cmp.setup({
-                                snippet = {
-                                        expand = function(args)
-                                                luasnip.lsp_expand(args.body) -- For 'luasnip' users
-                                                -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
-                                        end,
-                                },
-                                view = {
-                                        entries = "custom",
-                                        selection_order = "near_cursor",
-                                },
-                                window = {
-                                        completion = cmp.config.window.bordered(),
-                                        documentation = cmp.config.window.bordered(),
-                                },
-                                formatting = {
-                                        format = lspkind.cmp_format({
-                                                mode = "symbol_text",
-                                                menu = {
-                                                        buffer = "[Buffer]",
-                                                        nvim_lsp = "[LSP]",
-                                                        luasnip = "[LuaSnip]",
-                                                        nvim_lua = "[Lua]",
-                                                        latex_symbols = "[Latex]",
-                                                },
-                                        }),
-                                },
-                                mapping = cmp.mapping.preset.insert({
-                                        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                                        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+		---@module 'blink.cmp'
+		---@type blink.cmp.Config
+		opts = {
+			keymap = {
+				preset = "enter",
+				["<S-Tab>"] = { "select_prev", "fallback_to_mappings" },
+				["<Tab>"] = { "select_next", "fallback_to_mappings" },
+			},
 
-                                        ["<C-Space>"] = cmp.mapping.complete(),
-                                        ["<C-e>"] = cmp.mapping.abort(),
+			appearance = { nerd_font_variant = "mono" },
 
-                                        ["<CR>"] = cmp.mapping(function(fallback)
-                                                if cmp.visible() then
-                                                        if luasnip.expandable() then
-                                                                luasnip.expand()
-                                                        else
-                                                                cmp.confirm({
-                                                                        select = true,
-                                                                })
-                                                        end
-                                                else
-                                                        fallback()
-                                                end
-                                        end),
+			completion = {
+				menu = {
+					draw = {
+						-- We don't need label_description now because label and label_description are already
+						-- combined together in label by colorful-menu.nvim.
+						columns = { { "kind_icon" }, { "label", gap = 1 } },
+						components = {
+							kind_icon = {
+								text = function(ctx)
+									local icon = ctx.kind_icon
+									if vim.tbl_contains({ "Path" }, ctx.source_name) then
+										local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
+										if dev_icon then
+											icon = dev_icon
+										end
+									else
+										icon = require("lspkind").symbolic(ctx.kind, {
+											mode = "symbol",
+										})
+									end
 
-                                        ["<Tab>"] = cmp.mapping(function(fallback)
-                                                if cmp.visible() then
-                                                        cmp.select_next_item()
-                                                elseif luasnip.locally_jumpable(1) then
-                                                        luasnip.jump(1)
-                                                else
-                                                        fallback()
-                                                end
-                                        end, { "i", "s" }),
+									return icon .. ctx.icon_gap
+								end,
 
-                                        ["<S-Tab>"] = cmp.mapping(function(fallback)
-                                                if cmp.visible() then
-                                                        cmp.select_prev_item()
-                                                elseif luasnip.locally_jumpable(-1) then
-                                                        luasnip.jump(-1)
-                                                else
-                                                        fallback()
-                                                end
-                                        end, { "i", "s" }),
-                                }),
-                                sources = cmp.config.sources({
-                                        { name = "nvim_lsp" },
-                                        { name = "luasnip" },
-                                        { name = "nvim_lsp_signature_help" },
-                                        { name = "path" },
-                                }, {
-                                        { name = "buffer" },
-                                }),
+								highlight = function(ctx)
+									local hl = ctx.kind_hl
+									if vim.tbl_contains({ "Path" }, ctx.source_name) then
+										local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
+										if dev_icon then
+											hl = dev_hl
+										end
+									end
+									return hl
+								end,
+							},
 
-                                -- enable cmp-dap
-                                enabled = function()
-                                        return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt" or
-                                            require("cmp_dap").is_dap_buffer()
-                                end,
-                        })
+							label = {
+								text = function(ctx)
+									return require("colorful-menu").blink_components_text(ctx)
+								end,
+								highlight = function(ctx)
+									return require("colorful-menu").blink_components_highlight(ctx)
+								end,
+							},
+						},
+					},
+				},
+				ghost_text = { enabled = true },
+				list = { selection = { preselect = false } },
+				documentation = { auto_show = true, auto_show_delay_ms = 500 },
+			},
 
-                        -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-                        cmp.setup.cmdline({ "/", "?" }, {
-                                mapping = cmp.mapping.preset.cmdline(),
-                                sources = cmp.config.sources({
-                                        { name = "nvim_lsp_document_symbol" },
-                                }, {
-                                        { name = "buffer" },
-                                }),
-                        })
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer" },
+				providers = {
+					cmdline = {
+						min_keyword_length = function(ctx)
+							-- when typing a command, only show when the keyword is 3 characters or longer
+							if ctx.mode == "cmdline" and string.find(ctx.line, " ") == nil then
+								return 3
+							end
+							return 0
+						end,
+					},
+				},
+			},
 
-                        -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-                        cmp.setup.cmdline(":", {
-                                mapping = cmp.mapping.preset.cmdline(),
-                                sources = cmp.config.sources({
-                                        { name = "path" },
-                                }, {
-                                        { name = "cmdline" },
-                                }),
-                                matching = { disallow_symbol_nonprefix_matching = false },
-                        })
+			cmdline = {
+				keymap = { preset = "inherit" },
+				completion = {
+					menu = {
+						auto_show = function(ctx)
+							return vim.fn.getcmdtype() == ":"
+							-- enable for inputs as well, with:
+							-- or vim.fn.getcmdtype() == '@'
+						end,
+					},
+				},
+			},
 
-                        -- If you want insert `(` after select function or method item
-                        local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-                        cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-
-                        -- enable cmp-dap
-                        require("cmp").setup.filetype({ "dap-repl", "dapui_watches", "dapui_hover" }, {
-                                sources = {
-                                        { name = "dap" },
-                                },
-                        })
-                end,
-        },
-        {
-                -- for auto insert close pair
-                "windwp/nvim-autopairs",
-                event = "InsertEnter",
-                opts = {},
-        },
-        {
-                "danymat/neogen",
-                config = true,
-                -- Uncomment next line if you want to follow only stable versions
-                -- version = "*"
-        },
+			fuzzy = { implementation = "prefer_rust_with_warning" },
+		},
+		opts_extend = { "sources.default" },
+	},
+	{
+		-- for auto insert close pair
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		opts = {},
+	},
+	{
+		"danymat/neogen",
+		config = true,
+		-- Uncomment next line if you want to follow only stable versions
+		-- version = "*"
+	},
 }
