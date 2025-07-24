17697d008c8c5b7e24619b460a1a11be1e631836
Minh Nguyen <minh.nguyenthanh97@gmail.com>
2025-07-24 22:18:33 +0700

    add noice

--- nvim/.config/nvim/lua/plugins/00-display.lua
+++ nvim/.config/nvim/lua/plugins/00-display.lua
@@ -12,6 +12,28 @@ return {
 
         },
         {
+                "folke/noice.nvim",
+                event = "VeryLazy",
+                opts = {
+                        lsp = {
+                                -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+                                override = {
+                                        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                                        ["vim.lsp.util.stylize_markdown"] = true,
+                                        ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+                                },
+                        },
+                },
+                dependencies = {
+                        -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+                        "MunifTanjim/nui.nvim",
+                        -- OPTIONAL:
+                        --   `nvim-notify` is only needed, if you want to use the notification view.
+                        --   If not available, we use `mini` as the fallback
+                        "rcarriga/nvim-notify",
+                }
+        },
+        {
                 -- change status line display
                 "nvim-lualine/lualine.nvim",
                 dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -29,10 +51,15 @@ return {
                                                         path = 5, -- 1: Relative path
                                                 },
                                         },
-
-                                        lualine_x = { "filetype", "lsp_status" },
+                                        lualine_x = {
+                                                "filetype",
+                                                {
+                                                        require("noice").api.statusline.mode.get,
+                                                        cond = require("noice").api.statusline.mode.has,
+                                                        color = { fg = "#ff9e64" },
+                                                }
+                                        },
                                         lualine_y = { "progress" },
-
                                         lualine_z = { "location" },
                                 },
                         })
