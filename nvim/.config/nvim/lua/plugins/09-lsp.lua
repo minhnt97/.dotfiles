d28bc9f10e6278a3d405e6d24eca8998cdaf2cf3
Minh Nguyen <minh.nguyenthanh97@gmail.com>
2025-08-19 06:33:31 +0700

    integrate trouble with snacks

--- nvim/.config/nvim/lua/plugins/09-lsp.lua
+++ nvim/.config/nvim/lua/plugins/09-lsp.lua
@@ -60,6 +60,26 @@ return {
 	},
 	{
 		"folke/trouble.nvim",
+		specs = {
+			"folke/snacks.nvim",
+			opts = function(_, opts)
+				return vim.tbl_deep_extend("force", opts or {}, {
+					picker = {
+						actions = require("trouble.sources.snacks").actions,
+						win = {
+							input = {
+								keys = {
+									["<c-t>"] = {
+										"trouble_open",
+										mode = { "n", "i" },
+									},
+								},
+							},
+						},
+					},
+				})
+			end,
+		},
 		opts = {
 			auto_refresh = false, -- auto refresh when open
 			auto_close = true, -- auto close when there are no items
